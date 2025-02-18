5e9ce4acc42bcd9b747be6106a5dd1c11ead4942
Alan <Alanmuldashev@gmail.com>
2025-02-18 17:11:17 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int add(int lhs, int rhs);
+int subtract(int lhs, int rhs);
+
+int main() {
+
+    int lhs = 5;
+    int rhs = 3;
+
+    std::cout << add(lhs, rhs) << std::endl;
+    std::cout << subtract(lhs, rhs) << std::endl;
+}
